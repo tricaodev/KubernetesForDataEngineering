567f9c454189a2d6d3c257db9b2d3c2d20c922d6
Tri Cao <tricao.dev@gmail.com>
2025-05-23 18:04:54 +0700

    create DAG fetch_and_preview_data

---
+++ dags/fetch_and_preview_data.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+import requests
+
+def get_data(**kwargs):
+    url = "https://raw.githubusercontent.com/airscholar/ApacheFlink-SalesAnalytics/main/output/new-output.csv"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        df = pd.read_csv(url, header=None, names=['Category', 'Price', 'Quantity'])
+        json_data = df.to_json(orient='records')
+        kwargs['ti'].xcom_push(key='data', value=json_data)
+    else:
+        raise f"Failed to get data, HTTP status code: {response.status_code}"
+
+
+def preview_data(**kwargs):
+    json_data = kwargs['ti'].xcom_pull(key='data')
+    df = pd.DataFrame(json_data)
+    df['Total'] = df['Price'] * df['Quantity']
+    df.groupby('Category').agg({'Price': 'sum', 'Quantity': 'sum', 'Total': 'sum'})
+
+    print(df[['Category', 'Total']])
+
+default_args = {
+    'owner': 'tricao',
+    'start_date': datetime(2025,5,23, tzinfo=timezone(timedelta(hours=7)))
+}
+
+dag = DAG(
+    dag_id="fetch_and_preview_data",
+    default_args=default_args,
+    schedule=timedelta(days=1),
+    catchup=False,
+    is_paused_upon_creation=False
+)
+
+get_data = PythonOperator(
+    task_id="get_data",
+    python_callable=get_data
+)
+
+preview_data = PythonOperator(
+    task_id="preview_data",
+    python_callable=preview_data
+)
+
+get_data >> preview_data
